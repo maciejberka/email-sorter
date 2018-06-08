e8674533fe1d7f3832f2556c8d6735bf7e2d5798
maciejberka <maciejberka@gmail.com>
2018-06-08 20:08:30 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+//contain entered text
+let inputValue;
+
+//contain found emails
+let emails = [];
+
+const submitButton = document.querySelector('button');
+
+//take entered value to variable when user click at submit button
+submitButton.addEventListener('click', () => {
+  //clear previous array
+  emails = [];
+  
+  //get entered text
+  inputValue = document.querySelector('.text').value;
+  findEmails();
+});
+
+//this function looking for email adresses
+function findEmails() {
+  //take all words to the allWords array
+  let allWords = inputValue.split(' ');
+  
+  for(word of allWords){
+    //if found email adress and array doesn't contain it yet
+    if(word.includes('@') && !emails.includes(word)){
+      //add it to emails array
+      emails.push(word);
+    }
+  }
+  
+  sortByCompanyName();
+  
+};
+
+//sort emails alphabetical by word after '@'
+function sortByCompanyName() {
+  
+  emails.sort(function(a, b) {
+    return a.substr(a.indexOf('@')+1) > b.substr(b.indexOf('@')+1);
+  });
+  
+  printEmails();
+  
+}
+
+function printEmails(){
+  
+  const paragraph = document.querySelector('p');
+  
+  for(email of emails){
+    paragraph.innerHTML += `${email}<br>`
+  }
+  
+}
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
